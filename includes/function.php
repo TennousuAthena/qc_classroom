87c41d3005cd5a196a704e1c85968746f824483d
qctech <qc_d9f287b71882422ea956bf6fffb85d88@git.code.tencent.com>
2019-01-23 19:20:22 +0800

    新增函数库

---
+++ includes/function.php
@@ -0,0 +1,47 @@
+<?php
+/**
+ *  ___   ____ _
+ * / _ \ / ___| | __ _ ___ ___ _ __ ___   ___  _ __ ___
+ *| | | | |   | |/ _` / __/ __| '__/ _ \ / _ \| '_ ` _ \
+ *| |_| | |___| | (_| \__ \__ \ | | (_) | (_) | | | | | |
+ * \__\_\\____|_|\__,_|___/___/_|  \___/ \___/|_| |_| |_|
+ * 青草课堂 - function.php
+ * Copyright (c) 2015 - 2019.,QCTech ,All rights reserved.
+ * Created by: QCTech
+ * Created Time: 2019-01-23 - 18:26
+ */
+/**
+ * 获取用户真实ip
+ * @return bool
+ */
+function get_real_ip(){
+    $ip=false;
+    if(!empty($_SERVER["HTTP_CLIENT_IP"])){
+        $ip = $_SERVER["HTTP_CLIENT_IP"];
+    }
+    if (!empty($_SERVER['HTTP_X_FORWARDED_FOR'])) {
+        $ips = explode (", ", $_SERVER['HTTP_X_FORWARDED_FOR']);
+        if ($ip) { array_unshift($ips, $ip); $ip = FALSE; }
+        for ($i = 0; $i < count($ips); $i++) {
+            if (!eregi ("^(10|172\.16|192\.168)\.", $ips[$i])) {
+                $ip = $ips[$i];
+                break;
+            }
+        }
+    }
+    return ($ip ? $ip : $_SERVER['REMOTE_ADDR']);
+}
+
+/**
+ * SQL注入检查
+ * @param $Sql_Str
+ * @return string
+ */
+function inject_check($Sql_Str) {
+    $check=preg_match('/select|insert|update|delete|\'|\\*|\*|\.\.\/|\.\/|union|into|load_file|outfile/i',$Sql_Str);
+    if ($check) {
+        die();
+    }else{
+        return 1;
+    }
+}
