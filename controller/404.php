1c6d6db8d22a5deb462ed1cdb5b67c053cd60d2e
qctech <qc_d9f287b71882422ea956bf6fffb85d88@git.code.tencent.com>
2019-01-23 22:39:59 +0800

    新增404.php (感谢thinkphp)

---
+++ controller/404.php
@@ -0,0 +1,58 @@
+<?php
+/**
+ *  ___   ____ _
+ * / _ \ / ___| | __ _ ___ ___ _ __ ___   ___  _ __ ___
+ *| | | | |   | |/ _` / __/ __| '__/ _ \ / _ \| '_ ` _ \
+ *| |_| | |___| | (_| \__ \__ \ | | (_) | (_) | | | | | |
+ * \__\_\\____|_|\__,_|___/___/_|  \___/ \___/|_| |_| |_|
+ * 青草课堂 - 404.php
+ * Copyright (c) 2015 - 2019.,QCTech ,All rights reserved.
+ * Created by: QCTech
+ * Created Time: 2019-01-23 - 21:09
+ */
+//状态返回404
+http_response_code(404)
+?>
+<!DOCTYPE html>
+<html">
+<head>
+    <meta content="text/html; charset=utf-8" http-equiv="Content-Type">
+    <title>系统发生错误 ┭┮﹏┭┮</title>
+    <style type="text/css">
+        *{ padding: 0; margin: 0; }
+        html{ overflow-y: scroll; }
+        body{ background: #fff; font-family: '微软雅黑'; color: #333; font-size: 16px; }
+        img{ border: 0; }
+        .error{ padding: 24px 48px; }
+        .face{ font-size: 100px; font-weight: normal; line-height: 120px; margin-bottom: 12px; }
+        h1{ font-size: 32px; line-height: 48px; }
+        .error .content{ padding-top: 10px}
+        .error .info{ margin-bottom: 12px; }
+        .error .info .title{ margin-bottom: 3px; }
+        .error .info .title h3{ color: #000; font-weight: 700; font-size: 16px; }
+        .error .info .text{ line-height: 24px; }
+        .copyright{ padding: 12px 48px; color: #999; }
+        .copyright a{ color: #000; text-decoration: none; }
+    </style>
+</head>
+<body>
+<div class="error">
+    <p class="face">:(</p>
+    <h1>页面发生错误，请联系管理员</h1>
+    <div class="content">
+    </div>
+</div>
+<div class="copyright">
+    <p><a href="/">青草课堂</a><sup>Beta</sup> { 网络在线教育智能直播课堂解决方案 }</p>
+</div>
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-100755509-9"></script>
+<script>
+    window.dataLayer = window.dataLayer || [];
+    function gtag(){dataLayer.push(arguments);}
+    gtag('js', new Date());
+    gtag('config', 'UA-100755509-9');
+</script>
+</body>
+</body>
+</html>
+
