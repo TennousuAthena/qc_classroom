11e1554a712490f60e29999ac3d6a48871f828d6
qctech <qc_d9f287b71882422ea956bf6fffb85d88@git.code.tencent.com>
2019-01-29 21:44:07 +0800

    增加了用户信息的view层

---
+++ views/user/login.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ *  ___   ____ _
+ * / _ \ / ___| | __ _ ___ ___ _ __ ___   ___  _ __ ___
+ *| | | | |   | |/ _` / __/ __| '__/ _ \ / _ \| '_ ` _ \
+ *| |_| | |___| | (_| \__ \__ \ | | (_) | (_) | | | | | |
+ * \__\_\\____|_|\__,_|___/___/_|  \___/ \___/|_| |_| |_|
+ * 青草课堂 - login.php
+ * Copyright (c) 2015 - 2019.,QCTech ,All rights reserved.
+ * Created by: QCTech
+ * Created Time: 2019-01-29 - 16:46
+ */
+?>
+<div class="container main-content">
+    <div class="panel panel-primary">
+        <div class="panel-heading">
+            登录
+        </div>
+        <div class="panel-body">
+            <div class="container">
+                <div class="form-horizontal">
+                    <div class="form-group">
+                        <label class="col-sm-2 control-label">用户名</label>
+                        <div class="col-sm-10">
+                            <input type="text" class="form-control" name="" placeholder="请输入用户名" id="input_username">
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <label class="col-sm-2 control-label">密码</label>
+                        <div class="col-sm-10">
+                            <input type="password" class="form-control" name="" placeholder="请输入密码" id="input_password">
+                        </div>
+                    </div>
+
+
+                    <div class="form-group">
+                        <div class="col-sm-2">
+
+                        </div>
+                        <div class="col-sm-10">
+                            <button class="btn btn-primary">登录</button>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
