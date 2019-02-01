d8ff3b5155f54767f748f49474e1c9aa2dd35526
qctech <qc_d9f287b71882422ea956bf6fffb85d88@git.code.tencent.com>
2019-02-01 15:16:33 +0800

    初步写好登录页面，还需：引入gt,写好POST后功能

--- views/user/login.php
+++ views/user/login.php
@@ -11,3 +11,43 @@
  * Created Time: 2019-01-29 - 16:46
  */
 ?>
+    <div class="layui-card layui-container">
+        <div class="layui-card-header">登录</div>
+        <div class="layui-card-body">
+            <form class="layui-form" action="" method="post">
+                <div class="layui-form-item">
+                    <label class="layui-form-label">账号</label>
+                    <div class="layui-input-inline">
+                        <input type="text" name="username" id="username" required  lay-verify="required" placeholder="请输入您的账号" autocomplete="off" class="layui-input">
+                    </div>
+                    <div class="layui-form-mid layui-word-aux">支持您的用户名/手机号/邮箱</div>
+                </div>
+                <div class="layui-form-item">
+                    <label class="layui-form-label">密码</label>
+                    <div class="layui-input-inline">
+                        <input type="password" name="password" id="password" required lay-verify="required" placeholder="请输入您的密码" autocomplete="off" class="layui-input">
+                    </div>
+                </div>
+                <div class="layui-form-item">
+                    <label class="layui-form-label">记住我</label>
+                    <div class="layui-input-block">
+                        <input type="checkbox" name="switch" lay-skin="switch">
+                    </div>
+                    <div class="layui-form-mid layui-word-aux">不建议您在公共设备上启用</div>
+                </div>
+                <div class="layui-form-item">
+                    <label class="layui-form-label">验证码</label>
+                    <div id="embed-captcha"></div>
+                    <p id="wait" class="show">正在加载验证码......</p>
+                    <p id="notice" class="layui-hide">请先完成验证</p>
+                </div>
+                <div class="layui-form-item">
+                    <div class="layui-input-block">
+                        <button class="layui-btn" id="submit">提交</button>
+                        <button type="reset" class="layui-btn layui-btn-primary">重置</button>
+                    </div>
+                </div>
+            </form>
+        </div>
+    </div>
+    <?php $view->load_js("gt.js"); ?>
