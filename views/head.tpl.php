452a00ee17bf87d23cdafd1f949722459c3c27d2
qctech <qc_d9f287b71882422ea956bf6fffb85d88@git.code.tencent.com>
2019-02-01 10:41:26 +0800

    兼容IE8/9,支持媒体查询

--- views/head.tpl.php
+++ views/head.tpl.php
@@ -12,3 +12,8 @@
         <?php $view->load_js("jquery.min.js"); ?>
     </head>
     <body>
+    <!-- 让IE8/9支持媒体查询，从而兼容栅格 -->
+    <!--[if lt IE 9]>
+    <?php $view->load_js("html5.min.js"); ?>
+    <?php $view->load_js("respond.min.js"); ?>
+    <![endif]-->
