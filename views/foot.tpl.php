8c22e324b537f1348700d0dbc4c83d71313e29aa
qcminecraft <qcmc@qcminecraft.com>
2019-02-02 15:11:12 +0800

    foot新增错误弹窗处理

--- views/foot.tpl.php
+++ views/foot.tpl.php
@@ -2,6 +2,12 @@
     <?php $view->load_js("../layui.all.js"); ?>
     <?php $view->load_js("main.js"); ?>
 
+    <?php if(isset($ErrInfo)){?>
+        <script type="text/javascript">
+            layer.msg('<?php echo $ErrInfo;?>', {icon: 2});
+        </script>
+    <?php } ?>
+
 
     <?php $view->google_analytics(); ?>
     </body>
