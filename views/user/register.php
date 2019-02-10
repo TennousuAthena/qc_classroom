695ed65fe3dec8985ad0931b61036038de669ecf
qcminecraft <qcmc@qcminecraft.com>
2019-02-10 22:59:59 +0800

    完成用户注册前端页面

--- views/user/register.php
+++ views/user/register.php
@@ -19,4 +19,59 @@ if($Is_login){
     header("Location: /user/");
 }
 ?>
-
+<div class="layui-card layui-container">
+    <div class="layui-card-header">注册</div>
+    <div class="layui-card-body">
+        <form class="layui-form">
+            <div class="layui-form-item">
+                <label class="layui-form-label"><i class="layui-icon layui-icon-username"></i>用户名</label>
+                <div class="layui-input-inline">
+                    <input type="text" name="username" id="username" required  lay-verify="required" placeholder="请输入您的用户名" autocomplete="off" class="layui-input">
+                </div>
+            </div>
+            <div class="layui-form-item">
+                <label class="layui-form-label"><i class="layui-icon layui-icon-cellphone"></i>手机号</label>
+                <div class="layui-input-inline">
+                    <input type="text" name="phone" id="phone" required  lay-verify="required" placeholder="请输入您的手机号" autocomplete="off" class="layui-input">
+                </div>
+                <div class="layui-input-inline">
+                    <a class="layui-btn layui-btn-sm layui-btn-radius" id="sendSmsCode"></label>发送验证码</a>
+                </div>
+            </div>
+            <div class="layui-form-item">
+                <label class="layui-form-label"><i class="layui-icon layui-icon-vercode"></i>验证码</label>
+                <div class="layui-input-inline">
+                    <input type="number" name="smsCode" id="smsCode" required  lay-verify="required" placeholder="请输入您的短信验证码" autocomplete="off" class="layui-input">
+                </div>
+            </div>
+            <div class="layui-form-item">
+                <label class="layui-form-label"><i class="layui-icon layui-icon-password"></i>密码</label>
+                <div class="layui-input-inline">
+                    <input type="password" name="password" id="password" required lay-verify="required" placeholder="请输入您的密码" autocomplete="off" class="layui-input">
+                </div>
+            </div>
+            <div class="layui-form-item">
+                <label class="layui-form-label"><i class="layui-icon layui-icon-read"></i>年级</label>
+                <div class="layui-input-inline">
+                    <select id="educ" lay-filter="educ" name="educ" lay-search>
+                        <option value="">请选择你的学历</option>
+                        <option value="1">小学</option>
+                        <option value="2">初中</option>
+                        <option value="3">高中</option>
+                        <option value="4">大学</option>
+                        <option value="5">其他</option>
+                    </select>
+                </div>
+            </div>
+            <input type="text" style="display: none;" name="lid" id="lid" value="">
+            <div class="layui-form-item">
+                <div class="layui-input-block">
+                    <button type="button" class="layui-btn layui-btn-disabled" id="btn-register">提交</button>
+                    <button type="reset" class="layui-btn layui-btn-primary" id="btn-reset">重置</button>
+                </div>
+            </div>
+        </form>
+    </div>
+</div>
+<?php $view->load_js("gt.min.js"); ?>
+<?php $view->load_js("account.js"); ?>
