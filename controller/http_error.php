30f782e73767f1978178b7c3c68f1a7110f9dd2c
qcminecraft <qcmc@qcminecraft.com>
2019-02-13 19:33:50 +0800

    删除一个"

--- controller/http_error.php
+++ controller/http_error.php
@@ -17,7 +17,7 @@ if(!defined('DEBUG')) {
 }
 ?>
 <!DOCTYPE html>
-<html">
+<html>
 <head>
     <meta content="text/html; charset=utf-8" http-equiv="Content-Type">
     <meta name="viewport" content="width=device-width, initial-scale=1">
