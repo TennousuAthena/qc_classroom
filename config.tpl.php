92f1eb0b7ec95933bc29c2fe9de7e2f362c9e322
qcminecraft <qcmc@qcminecraft.com>
2019-02-13 22:03:26 +0800

    修复Access Denied的BUG

--- config.tpl.php
+++ config.tpl.php
@@ -10,11 +10,6 @@
  * Created by: QCTech
  * Created Time: 2019-01-23 - 12:24
  */
-//防止被恶意访问，泄露信息
-if(!defined('DEBUG')) {
-    http_response_code(403);
-    exit('Access Denied');
-}
 // -=这里是青草课堂的配置文件=-
 // 注意：所有项目都是必填项，否则程序无法正常运行！
 DEFINE("DEBUG", true);   //是否开启调试模式，建议不在生产环境中启用
