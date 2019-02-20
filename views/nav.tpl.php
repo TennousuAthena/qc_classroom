954f1924dfe8a379db66ca4cd1b87824fa75f248
qcminecraft <qcmc@qcminecraft.com>
2019-02-20 17:27:13 +0800

    新增noscript提示

--- views/nav.tpl.php
+++ views/nav.tpl.php
@@ -19,8 +19,11 @@ $usercenter = new usercenter();
 ?>
 <header>
     <div class="layui-hide-lg layui-hide-md layui-hide-sm layui-bg-orange" style="line-height: 2em">
-        <p> <i class="layui-icon layui-icon-about"></i> 请注意：青草课堂的设计没有考虑移动端适配，若有页面问题请及时<a href="https://gitee.com/qcmc/qc_classrom/issues" target="_blank" rel="nofollow">反馈</a>！</p>
+        <p> <i class="layui-icon layui-icon-about"></i> 请注意：青草课堂的设计没有考虑移动端适配，若有页面问题请及时<a href="https://github.com/qcminecraft/qc_classrom/issues" target="_blank">反馈</a>！</p>
     </div>
+    <noscript>
+            <i class="layui-icon layui-icon-about"></i> 使用青草课堂<b style="color:#f00;">必须</b>开启JavaScript，否则无法使用！
+    </noscript>
     <div class="layui-layout layui-layout-admin">
         <div class="layui-header">
             <div class="layui-logo"><a href="/"><img src="<?php echo $Config["website"]["static"]; ?>img/logo_.png" style="height: 60px;"></a> </div>
