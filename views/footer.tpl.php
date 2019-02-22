06bddabdb845d150a77713dfb4fe2386a4e3bffe
qcminecraft <qcmc@qcminecraft.com>
2019-02-22 20:12:16 +0800

    在脚部添加使用必读链接

--- views/footer.tpl.php
+++ views/footer.tpl.php
@@ -20,6 +20,7 @@ if(!defined('DEBUG')) {
 <footer class="qc_footer">
     <div class="layui-bg-gray" style="line-height: 5em">
         <p style="text-align: center;"> &copy <a href="https://blog.qmcmc.cn" target="_blank" title="前往作者Blog"> 青草</a> ·<a href="https://gitee.com/qcmc/qc_classrom" target="_blank" rel="nofollow" title="查看项目源代码"> 课堂 </a>
-            2016-<?php echo date('Y');?> All rights reserved <img src="https://circleci.com/gh/qcminecraft/qc_classrom.svg?style=svg&circle-token=e33f17a2b23f23e80f9d31d6e34b0ff898fd3ff4" alt="项目构建状态" title="构建状态"><br />
+            2016-<?php echo date('Y');?> All rights reserved | <a href="/doc/before-you-use" target="_blank">使用必读</a> |
+            <img src="https://circleci.com/gh/qcminecraft/qc_classrom.svg?style=svg&circle-token=e33f17a2b23f23e80f9d31d6e34b0ff898fd3ff4" alt="项目构建状态" title="构建状态"><br />
     </div>
 </footer>
