eb9dfe3ebd0774e47b1658fffcab275c17fe9cdc
qcminecraft <qcmc@qcminecraft.com>
2019-03-10 16:14:39 +0800

    新增了pjax的类，为pjax开发做准备，闭合了p标签

--- views/nav.tpl.php
+++ views/nav.tpl.php
@@ -17,52 +17,53 @@ if(!defined('DEBUG')) {
 }
 $usercenter = new usercenter();
 ?>
-<header>
-    <div class="layui-hide-lg layui-hide-md layui-hide-sm layui-bg-orange" style="line-height: 2em">
-        <p> <i class="layui-icon layui-icon-about"></i> 请注意：青草课堂的设计没有考虑移动端适配，若有页面问题请及时<a href="https://github.com/qcminecraft/qc_classrom/issues" target="_blank">反馈</a>！</p>
-    </div>
-    <noscript>
-            <i class="layui-icon layui-icon-about"></i> 使用青草课堂<b style="color:#f00;">必须</b>开启JavaScript，否则无法使用！
-    </noscript>
-    <div class="layui-layout layui-layout-admin">
-        <div class="layui-header">
-            <div class="layui-logo"><a href="/"><img src="<?php echo $Config["website"]["static"]; ?>img/logo_.png" style="height: 60px;"></a> </div>
-            <ul class="layui-nav layui-layout-left layui-hide-xs">
-                <li class="layui-nav-item <?php if($_SERVER['REQUEST_URI'] === '/') echo "layui-this" ?>"><a href="/"><em class="layui-icon layui-icon-home"> </em>首页</a></li>
-                <?php if($usercenter->get_user_group($conn, $Uid) > 1){ ?>
-                    <li class="layui-nav-item <?php if($_SERVER['REQUEST_URI'] === '/teacher/createCourse') echo "layui-this" ?>"><a href="/teacher/createCourse"><em class="layui-icon layui-icon-add-1"> </em>创建课程</a></li>
-                <?php }?>
-                <?php if($usercenter->get_user_group($conn, $Uid) > 1){ ?>
-                    <li class="layui-nav-item <?php if($_SERVER['REQUEST_URI'] === '/teacher/') echo "layui-this" ?>">
-                    <a href="javascript:;"><em class="layui-icon layui-icon-username"> </em> 教师中心</a>
-                    <dl class="layui-nav-child"> <!-- 二级菜单 -->
-                        <dd><a href="">创建课程</a></dd>
-                        <dd><a href="">创建直播</a></dd>
-                    </dl>
-                <?php } ?>
-                <li class="layui-nav-item <?php if($_SERVER['REQUEST_URI'] === '/user/myCourse') echo "layui-this" ?>"><a href="/user/myCourse"><em class="layui-icon layui-icon-list"> </em> 我的课程</a></li>
-            </ul>
-            <ul class="layui-nav layui-layout-right">
-                <li class="layui-nav-item">
-                    <a href="<?php if(!$Is_login){ echo "/user/login"; }else{?>javascript:;<?php } ?>">
-                        <img src="<?php echo $Config["website"]["static"] . $usercenter->get_avatar($Uid, $conn); ?>" class="layui-nav-img">
-                        <?php echo $Uinfo['username']; ?>
-                    </a>
-                    <dl class="layui-nav-child">
-                        <?php if($Is_login){ ?>
-                            <?php if($usercenter->get_user_group($conn, $Uid) == 1){ ?>
-                        <dd><a href="###" lay-filter="become_teacher">成为老师</a> </dd>
-                            <?php } ?>
-                        <dd><a href="###">基本资料</a></dd>
-                        <dd><a href="###">安全设置</a></dd>
-                        <dd><a href="/user/logout">退出登录</a></dd>
-                        <?php }else{ ?>
-                        <dd><a href="/user/login">登录</a></dd>
-                        <dd><a href="/user/register">注册</a></dd>
-                        <?php } ?></dl>
-                </li>
-            </ul>
+    <header>
+        <div class="layui-hide-lg layui-hide-md layui-hide-sm layui-bg-orange" style="line-height: 2em">
+            <p> <i class="layui-icon layui-icon-about"></i> 请注意：青草课堂的设计没有考虑移动端适配，若有页面问题请及时<a href="https://github.com/qcminecraft/qc_classrom/issues" target="_blank">反馈</a>！</p>
         </div>
-    </div>
-    <div style="height: 2em"></div>
-</header>
+        <noscript>
+                <i class="layui-icon layui-icon-about"></i> 使用青草课堂<b style="color:#f00;">必须</b>开启JavaScript，否则无法使用！
+        </noscript>
+        <div class="layui-layout layui-layout-admin">
+            <div class="layui-header">
+                <div class="layui-logo"><a href="/"><img src="<?php echo $Config["website"]["static"]; ?>img/logo_.png" style="height: 60px;"></a> </div>
+                <ul class="layui-nav layui-layout-left layui-hide-xs">
+                    <li class="layui-nav-item <?php if($_SERVER['REQUEST_URI'] === '/') echo "layui-this" ?>"><a href="/"><em class="layui-icon layui-icon-home"> </em>首页</a></li>
+                    <?php if($usercenter->get_user_group($conn, $Uid) > 1){ ?>
+                        <li class="layui-nav-item <?php if($_SERVER['REQUEST_URI'] === '/teacher/createCourse') echo "layui-this" ?>"><a href="/teacher/createCourse"><em class="layui-icon layui-icon-add-1"> </em>创建课程</a></li>
+                    <?php }?>
+                    <?php if($usercenter->get_user_group($conn, $Uid) > 1){ ?>
+                        <li class="layui-nav-item <?php if($_SERVER['REQUEST_URI'] === '/teacher/') echo "layui-this" ?>">
+                        <a href="javascript:;"><em class="layui-icon layui-icon-username"> </em> 教师中心</a>
+                        <dl class="layui-nav-child"> <!-- 二级菜单 -->
+                            <dd><a href="">创建课程</a></dd>
+                            <dd><a href="">创建直播</a></dd>
+                        </dl>
+                    <?php } ?>
+                    <li class="layui-nav-item <?php if($_SERVER['REQUEST_URI'] === '/user/myCourse') echo "layui-this" ?>"><a href="/user/myCourse"><em class="layui-icon layui-icon-list"> </em> 我的课程</a></li>
+                </ul>
+                <ul class="layui-nav layui-layout-right">
+                    <li class="layui-nav-item">
+                        <a href="<?php if(!$Is_login){ echo "/user/login"; }else{?>javascript:;<?php } ?>">
+                            <img src="<?php echo $Config["website"]["static"] . $usercenter->get_avatar($Uid, $conn); ?>" class="layui-nav-img">
+                            <?php echo $Uinfo['username']; ?>
+                        </a>
+                        <dl class="layui-nav-child">
+                            <?php if($Is_login){ ?>
+                                <?php if($usercenter->get_user_group($conn, $Uid) == 1){ ?>
+                            <dd><a href="###" lay-filter="become_teacher">成为老师</a> </dd>
+                                <?php } ?>
+                            <dd><a href="###">基本资料</a></dd>
+                            <dd><a href="###">安全设置</a></dd>
+                            <dd><a href="/user/logout">退出登录</a></dd>
+                            <?php }else{ ?>
+                            <dd><a href="/user/login">登录</a></dd>
+                            <dd><a href="/user/register">注册</a></dd>
+                            <?php } ?></dl>
+                    </li>
+                </ul>
+            </div>
+        </div>
+        <div style="height: 2em"></div>
+    </header>
+    <div id="pjax-content">
