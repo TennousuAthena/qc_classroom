56a44b5980c8ea8ce9560f91d7725d404972c612
qcminecraft <qcmc@qcminecraft.com>
2019-03-26 21:44:55 +0800

    更新后端服务

--- server/server.php
+++ server/server.php
@@ -11,19 +11,4 @@
  * Created Time: 2019-02-26 - 15:13
  */
 
-require_once ('../includes/MeepoPS/index.php');
-
-//使用WebSocket协议传输的Api类
-$webSocket = new \MeepoPS\Api\Websocket('0.0.0.0', '23333');
-$webSocket->callbackNewData = function ($connect, $data){
-    $msg = '收到用户发送的消息: ' . $data;
-    $message = array(
-        'errno' => 0, 'errmsg' => 'OK', 'data' => array(
-            'content' => $msg, 'create_time' => date('Y-m-d H:i:s'),
-        ),
-    );
-    $connect->send(json_encode($message));
-};
-
-//启动MeepoPS
-\MeepoPS\runMeepoPS();
+//Noting here~
