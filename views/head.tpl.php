6f1b07f51d457bf16697dd1c7f1513ee984dff5e
qcminecraft <qcmc@qcminecraft.com>
2019-03-28 22:28:50 +0800

    修复：dplayer无法显示

--- views/head.tpl.php
+++ views/head.tpl.php
@@ -57,7 +57,7 @@ if(!@$title){
 
     <?php $view->load_js("../layui.js"); ?>
     <?php $view->load_js("main.js", true); ?>
-    <?php $view->load_js("dplayer.min.js", true); ?>
+    <?php $view->load_js("dplayer.min.js"); ?>
 
     <script type="text/javascript">
         const Static = '<?php echo $Config["website"]["static"] ?>';
